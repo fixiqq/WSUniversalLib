7f7368334d2a2bb5a442ceffbdf496125b5a553c
калентьевсд <@211-01>
2023-03-25 10:58:43 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/AgentPriorityTests.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSUniversalLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AgentPriorityTests
+    {
+        Calculation calculation = new Calculation();
+        [TestMethod]
+        public void GetPriorityForAgent_ValidParameters_ReturnsExpectedResult()
+        {
+
+            float age = 23;
+            int experience = 2;
+            int agentType = 2;
+            int expectedPriority = 7;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        [TestMethod]
+        public void GetPriorityForAgent_ValidParameters_ReturnsExpectedResult2()
+        {
+
+            float age = 23;
+            int experience = 1;
+            int agentType = 2;
+            int expectedPriority = 3;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentTypeNotSupported_ReturnsMinusOne()
+        {
+
+            float age = 35;
+            int experience = 12;
+            int agentType = 4;
+            int expectedPriority = -1;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentTypeNotSupported_ReturnsMinusOne2()
+        {
+
+            float age = 35;
+            int experience = 12;
+            int agentType = 0;
+            int expectedPriority = -1;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult()
+        {
+
+            float age = 22;
+            int experience = 1;
+            int agentType = 1;
+            int expectedPriority = 2;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult2()
+        {
+
+            float age = 25;
+            int experience = 1;
+            int agentType = 1;
+            int expectedPriority = 2;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult5()
+        {
+
+            float age = 22;
+            int experience = 2;
+            int agentType = 3;
+            int expectedPriority = 9;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult6()
+        {
+
+            float age = 22;
+            int experience = 3;
+            int agentType = 3;
+            int expectedPriority = 13;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult4()
+        {
+
+            float age = 22;
+            int experience = 1;
+            int agentType = 3;
+            int expectedPriority = 4;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+
+        [TestMethod]
+        public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult3()
+        {
+
+            float age = 27;
+            int experience = 2;
+            int agentType = 1;
+            int expectedPriority = 4;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+        [TestMethod]
+            public void GetPriorityForAgent_AgentIsYoung_ReturnsExpectedResult7()
+        {
+
+            float age = 27;
+            int experience = 2;
+            int agentType = 2;
+            int expectedPriority = 6;
+
+
+            int priority = calculation.GetPriorityForAgent(age, experience, agentType);
+
+
+            Assert.AreEqual(expectedPriority, priority);
+        }
+
+
+
+    }
+}
---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public int GetPriorityForAgent(float age, float experience, int agentType)
+        {
+            double agentCoefficient = 0.0;
+
+            switch (agentType)
+            {
+                case 1:
+                    agentCoefficient = 1.8;
+                    break;
+                case 2:
+                    agentCoefficient = 3.2;
+                    break;
+                case 3:
+                    agentCoefficient = 4.1;
+                    break;
+                default:
+                    return -1;
+            }
+
+            if (experience > 40)
+            {
+                agentCoefficient += 0.9;
+            }
+            else if (experience > 20)
+            {
+                agentCoefficient += 0.7;
+            }
+            else if (experience > 10)
+            {
+                agentCoefficient += 0.5;
+            }
+
+            if (age <= 25 && experience <= 3)
+            {
+                switch (agentType)
+                {
+                    case 1:
+                        agentCoefficient += 0.1;
+                        break;
+                    case 2:
+                        agentCoefficient += 0.17;
+                        break;
+                    case 3:
+                        agentCoefficient += 0.26;
+                        break;
+                }
+            }
+
+            double agentPriority = agentCoefficient * experience;
+            int roundedPriority = (int)Math.Round(agentPriority);
+
+            return roundedPriority;
+        }
+    }
+}
